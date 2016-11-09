03432cceb70acf8b4cd87486509b4389450b57a0
KeaneTK <keane.tk@gmail.com>
2016-11-09 16:17:59 -0600

    Rolling my original experiment into Reagan's site.

--- HCI_Group24_Edit.js
+++ HCI_Group24_Edit.js
@@ -14,5 +14,29 @@ function loadNews() {
 }
 window.onload = loadNews;
 
+function saveNews() {
+	var title = document.getElementById("editTitle").value;
+	var text = document.getElementById("editBody").value;
+	
+	newsJson = newsJson.substring(0, newsJson.length - 2) + ",{\"title\":\"" + title + "\", \"text\":\"" + text + "\"}]}";
+	loadNews();
+	$("#NewsEdit").modal("hide");
+}
+
+function modalExerciseLoad() {
+	var exerciseItem = JSON.parse(exerciseJson);
+	document.getElementById("editExercise1").value = exerciseItem.exercise.title;
+	document.getElementById("editExercise2").value = exerciseItem.exercise.text;
+}
+
+function saveExercise() {
+	var title = document.getElementById("editExercise1").value;
+	var text = document.getElementById("editExercise2").value;
+	
+	exerciseJson = "{\"exercise\":{\"title\":\"" + title + "\", \"text\":\"" + text + "\"}}";
+	loadNews();
+	$("#ExerciseEdit").modal("hide");
+}
+
 var newsJson = '{"news":[{"title":"New Website!", "text":"This is our new website.  Hope it works."},{"title":"Continuing Work", "text":"We are continuing our work here.  Look at the functional Twitter feed.  Facebook doesn\'t work with local pages though."}]}';
 var exerciseJson = '{"exercise":{"title":"Squats", "text":"15 reps, 2 sets"}}';
---
+++ Login.js
@@ -0,0 +1,24 @@
+function loginClick() {
+	var username = document.getElementById("userName").value;
+	var password = document.getElementById("password").value;
+	
+	if (names.includes(username)) {
+		var index = names.indexOf(username);
+		if (pass[index] == password) {
+			if(username === "Coach" && password === "gym") {
+				window.location.href = "HCI_Group24_Edit.html";
+			} else {
+				window.location.href = "HCI_Group24.html";
+			}
+		} else {
+			document.getElementsByClassName("alert-danger")[0].style.display = 'block';
+			document.getElementById("password").value = "";
+		}
+	} else {
+		document.getElementsByClassName("alert-danger")[0].style.display = 'block';
+		document.getElementById("password").value = "";
+	}
+}
+
+var names = ["Keane", "Reagan", "Steven", "Kendel", "Coach"];
+var pass = ["hciprojectt", "hciprojectr", "hciprojects", "hciprojectk", "gym"];
